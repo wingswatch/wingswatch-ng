81911b545458d40a0a64095c7aebc3a7ac209207
Patrick Sarnowski <psarnowski@gmail.com>
2021-04-05 17:27:08 -0400

    Add EventSearchResult for search API.

---
+++ src/app/models/event-search-result.ts
@@ -0,0 +1,13 @@
+export interface EventSearchResult {
+    eventId: string;
+    ntsbNumber: string;
+    eventType: string;
+    eventDate: string;
+    make: string;
+    model: string;
+    city: string;
+    state: string;
+    country: string;
+    injurySeverity: string;
+    location: string;
+}
