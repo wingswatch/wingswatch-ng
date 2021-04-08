e2852b96bb2f32cd49c00b4bc7c803df4b6358fa
Patrick Sarnowski <psarnowski@gmail.com>
2021-04-08 18:21:08 -0400

    Clean up home component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  body: HTMLBodyElement = document.getElementsByTagName('body')[0];
-
-  ngOnInit() {
-    this.body.setAttribute('id', 'homepage');
-  }
-
-  ngOnDestroy() {
-    this.body.removeAttribute('id');
-  }
-
-}
+export class HomeComponent { }
