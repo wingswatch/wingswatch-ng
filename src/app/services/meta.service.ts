2cb8aa8b3fd2a06b585f837164fa7dacd8a42e40
Patrick Sarnowski <psarnowski@gmail.com>
2022-04-14 04:33:12 -0400

    Add a service to handle meta tags and title.

---
+++ src/app/services/meta.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MetaService {
+    constructor(
+        private titleService: Title,
+        private meta: Meta,
+    ) { }
+
+    updateMetaInfo(content: string, keywords: string): void {
+        console.log('ok');
+        this.meta.updateTag({ name: 'description', content: content });
+        //this.meta.updateTag({ name: 'author', content: author });
+        this.meta.updateTag({ name: 'keywords', content: keywords });
+    }
+
+    updateTitle(title?: string): void {
+        this.titleService.setTitle(title + ' | WingsWatch');
+    }
+}
